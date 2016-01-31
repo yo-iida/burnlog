6a482c1e67a35196856f2914a6c2964d0996bb52
飯田 意己 <lohocla@icloud.com>
2016-01-31 18:08:39 +0900

    マイグレーションをdeviseに合わせて修正

--- db/migrate/20160124144534_create_users.rb
+++ db/migrate/20160124144534_create_users.rb
@@ -2,10 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.string :email
-      t.string :password
-
-      t.timestamps null: false
+      t.string :provider
+      t.string :uid
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160124144534) do
+ActiveRecord::Schema.define(version: 20160130062742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "email",               default: "", null: false
+    t.string   "encrypted_password",  default: ""
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",       default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
