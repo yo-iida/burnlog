a2f800eba84d386173d88788442a3056652fea51
飯田 意己 <lohocla@icloud.com>
2016-01-31 18:16:14 +0900

    flash追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def flash_icon_class(name)
+    case name
+      when :error
+        'icon-exclamation-sign'
+      when :alert
+        'icon-warning-sign'
+      when :notice
+        'icon-info-sign'
+      when :success
+        'icon-ok'
+    end
+  end
+  
 end
