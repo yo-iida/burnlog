ebcd861306fb1bc48e79c69fed3f11f1e15eb9d9
Yoshiki Iida <yoshiki.iida@crowdworks.co.jp>
2016-06-19 23:14:57 +0900

    oauthで取得したaccess tokenでOctokit::Clientを利用できるようにする

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,4 +1,9 @@
 class DefaultController < ApplicationController
   def index
+    if session[:access_token]
+      token = session[:access_token]
+      client = Octokit::Client.new access_token: token
+      @repos = client.repos
+    end
   end
 end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_or_create_by(user_params)
 
     if @user.persisted?
+      session[:access_token] = request.env["omniauth.auth"]["credentials"]["token"]
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
     else
