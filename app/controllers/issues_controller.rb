faf77c8a788e12e982a08b819d36cb29170d0fc8
Yoshiki Iida <yoshiki.iida@crowdworks.co.jp>
2016-06-27 10:47:25 +0900

    ポイントが振られたclosed_issuesを表示できるように

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -10,7 +10,8 @@ class IssuesController < ApplicationController
       @sprint_backlog_issues = client.issues(params["repo"], state: 'open', labels: 'sprint backlog')
       @in_progress_issues = client.issues(params["repo"], state: 'open', labels: 'in progress')
       @in_review_issues = client.issues(params["repo"], state: 'open', labels: 'in review')
-      @closed_issues = client.issues(params["repo"], state: 'closed', since: Date.today.beginning_of_week)
+      @closed_issues = client.issues(params["repo"], state: 'closed', since: base_day.beginning_of_week)
+      @closed_issues_with_point = @closed_issues.select { |i| i.labels.map(&:name).to_s.include?('point') }
     end
   end
 end
